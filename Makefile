c0bb8f124a6835a1ee9dc6c2a0531931b8dc52e9
Johnothan King <johnothanking@protonmail.com>
2017-06-22 17:06:09 -0700

    Initial commit
    Signed-off-by: Johnothan King <johnothanking@protonmail.com>

---
+++ Makefile
@@ -0,0 +1,48 @@
+# Copyright (c) 2017 Johnothan King. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+CC := cc
+WFLAGS  := -Wall -Wextra -Wpedantic
+CFLAGS  := -O2 -ffast-math -fomit-frame-pointer -fstack-protector-strong -fstack-check -fPIC -flto
+LDFLAGS := -Wl,--sort-common,--as-needed,-O1,-z,relro,-z,now $(CFLAGS)
+
+all:
+	$(CC) $(WFLAGS) $(CFLAGS) $(CPPFLAGS) -o init init.c $(LDFLAGS)
+
+install:
+	mkdir -p $(DESTDIR)/sbin $(DESTDIR)/etc/leaninit
+	install -Dm0755 init $(DESTDIR)/sbin/l-init
+	install -Dm0755 halt $(DESTDIR)/sbin/l-halt
+	install -Dm0755 reboot $(DESTDIR)/sbin/l-reboot
+	install -Dm0755 lsvc $(DESTDIR)/sbin
+	install -Dm0755 rc $(DESTDIR)/etc/leaninit
+	install -Dm0644 ttys $(DESTDIR)/etc/leaninit
+	ln -sr $(DESTDIR)/sbin/l-halt $(DESTDIR)/sbin/l-poweroff
+	sed -i 's:/etc/ttys:/etc/leaninit/ttys:g' $(DESTDIR)/etc/leaninit/rc
+	sed -i 's:exec init:exec l-init:g' $(DESTDIR)/sbin/l-halt
+	sed -i 's:exec halt:exec l-halt:g' $(DESTDIR)/sbin/l-reboot
+
+clean:
+	rm -f init
+
+clobber: clean
+
+mrproper: clean
---
+++ init.c
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2017 Johnothan King. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * A lean replacement for init
+ */
+
+#include <sys/reboot.h>
+#include <stdio.h>
+#include <unistd.h>
+
+// This function should never stop
+static inline void rc(const char *m)
+{
+#ifdef OVERRIDE
+    execl("/bin/sh", "/bin/sh", "/etc/rc", m, NULL);
+#else
+    execl("/bin/sh", "/bin/sh", "/etc/leaninit/rc", m, NULL);
+#endif
+
+    // In case the function stops
+    sync();
+}
+
+// The main function
+int main(int argc, char *argv[])
+{
+    // Execute the init script /etc/rc
+    if(argc == 1) {
+        rc("v");
+
+    // SysV-like shutdown and reboot
+    } else {
+        // Synchronize the filesystems to prevent data corruption
+        sync();
+
+        // Determine what to do
+        switch(*argv[1]) {
+            case '0':
+                reboot(RB_POWER_OFF);
+                return 0;
+            case '6':
+                reboot(RB_AUTOBOOT);
+                return 0;
+            case 'q': // For quiet boot
+                rc("q");
+            case 's': // Defaults to quiet boot
+                rc("q");
+            case 'v': // Verbose boot (default)
+                rc("v");
+            default:
+                printf("Argument invalid! The only accepted arguments are '0' and '6'\n");
+                return 1;
+        }
+    }
+}
