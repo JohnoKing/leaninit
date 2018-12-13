f3bdfdea0940bd6ae83bb32666b8167eff8d8349
Johnothan King <johnothanking@protonmail.com>
2018-12-12 16:14:16 -0800

    Avoid passing -p to /bin/login when it is not needed
    Signed-off-by: Johnothan King <johnothanking@protonmail.com>

--- cmd/lgetty.c
+++ cmd/lgetty.c
@@ -41,12 +41,19 @@ int main(int argc, char *argv[])
 	// Find login(1)
 	char login_cmd[19];
 	if(access("/bin/login", X_OK) == 0)
-		memcpy(login_cmd, "/bin/login -p ", 15);
+		memcpy(login_cmd, "/bin/login ", 12);
 	else if(access("/usr/bin/login", X_OK) == 0)
-		memcpy(login_cmd, "/usr/bin/login -p ", 19);
+		memcpy(login_cmd, "/usr/bin/login ", 16);
 	else {
-		printf(RED "* Could not find login(1) (please symlink it to either /bin/login or /usr/bin/login)" RESET "\n");
+		printf(RED "* Could not find login(1) (please symlink it to either /bin/login or /usr/bin/login and give it executable permissions)" RESET "\n");
 		return 127;
+	}
+
+	// Pass -p to login when debug mode is enabled
+	unsigned int debug = 1;
+	if((argc > 2) && (strcmp(argv[2], "debug") == 0)) {
+		strncat(login_cmd, "-p ", 4);
+		debug = 0;
 	}
 
 	// Infinite loop
@@ -81,14 +88,16 @@ int main(int argc, char *argv[])
 			memcpy(cmd,  login_cmd, 19);
 			strncat(cmd, input,    100);
 
-			// Because -p is passed to login(1) to preserve the environment, $HOME must be corrected
-			if(strcmp("root", input) == 0)
-				putenv("HOME=/root");
-			else {
-				char home[112];
-				memcpy(home, "HOME=/home/", 12);
-				strncat(home, input, 100);
-				putenv(home);
+			// Because -p is passed to login(1) to preserve the environment, $HOME must be corrected (for debug mode)
+			if(debug == 0) {
+				if(strcmp("root", input) == 0)
+					putenv("HOME=/root");
+				else {
+					char home[112];
+					memcpy(home, "HOME=/home/", 12);
+					strncat(home, input, 100);
+					putenv(home);
+				}
 			}
 
 			// Execute login(1)
