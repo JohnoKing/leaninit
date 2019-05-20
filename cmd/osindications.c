479b533e7ea547fc0f1e0685fdeaca13e57623ea
Johnothan King <johnothanking@protonmail.com>
2019-05-19 21:53:54 -0700

    Rename osindications-set to osindications, add error checks and comments to osindications.c
    Signed-off-by: Johnothan King <johnothanking@protonmail.com>

--- cmd/osindications.c
+++ cmd/osindications.c
@@ -21,19 +21,34 @@
  */
 
 /*
- * osindications-set - This program sets OsIndications for booting into firmware setup.
+ * osindications - This program sets OsIndications for booting into firmware setup.
  */
 
-#include <stdio.h>
-#include <unistd.h>
+#include "inc.h"
 
 int main(void) {
+
+	// Error checks
+	if(getuid() != 0) {
+		printf(RED "* Permission denied!" RESET "\n");
+		return 1;
+	else if(access("/sys/firmware/efi/efivars/OsIndicationsSupported-8be4df61-93ca-11d2-aa0d-00e098032b8c", W_OK) != 0) {
+		printf(RED "* This system does not support OsIndications!" RESET "\n");
+		return 1;
+	}
+
+	// OsIndications variables
 	unsigned long efi_attr = 0x0000000000000007;
 	unsigned long efi_boot = 0x0000000000000001;
+
+	// Write to OsIndications
 	FILE *fd = fopen("/sys/firmware/efi/efivars/OsIndications-8be4df61-93ca-11d2-aa0d-00e098032b8c", "w+");
 	fwrite(&efi_attr, 1, 4, fd);
 	fwrite(&efi_boot, 1, 1, fd);
 	fclose(fd);
 	sync();
+
+	// Notify the user of the change
+	printf(CYAN "* " WHITE "This system will now boot into the firmware's UI the next time it boots." RESET "\n");
 	return 0;
 }
