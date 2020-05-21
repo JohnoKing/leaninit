9d98fe420cbf13101ce01e86efc37191fc338570
Johnothan King <johnothanking@protonmail.com>
2020-05-21 09:31:59 -0700

    Improve the error handling for single user mode fallback
    Signed-off-by: Johnothan King <johnothanking@protonmail.com>

--- cmd/init.c
+++ cmd/init.c
@@ -169,7 +169,7 @@ static void multi(void)
     char *rc = get_file_path("/etc/leaninit/rc", "/etc/rc", X_OK);
     if(rc == NULL) {
         printf(PURPLE "* " YELLOW "Neither /etc/rc or /etc/leaninit/rc could be found, falling back to single user mode..." RESET "\n");
-        flags ^= SINGLE_USER;
+        flags &= ~(SINGLE_USER);
         return single();
     }
 
@@ -177,7 +177,7 @@ static void multi(void)
     if((flags & VERBOSE) == VERBOSE) printf(CYAN "* " WHITE "Executing %s..." RESET "\n", rc);
     if(sh(rc) != 0) {
         printf(PURPLE "* " YELLOW "%s has failed, falling back to single user mode..." RESET "\n", rc);
-        flags ^= SINGLE_USER;
+        flags &= ~(SINGLE_USER);
         return single();
     }
 
@@ -350,7 +350,7 @@ int main(int argc, char *argv[])
         int stored_signal;
         while(true) {
 
-            // Wait for a signal, then store it in stored_signal to prevent race conditions
+            // Wait for a signal, then store it to prevent race conditions
             pause();
             stored_signal = current_signal;
 
